61f630cb7ac22b9d2d32b21f3c331f0e5e00dfe3
saarikoski-jules <jsaariko@student.codam.nl>
2020-10-14 12:23:29 +0200

    program displays prompt and prints given input

---
+++ includes/minishell.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   minishell.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/14 12:02:24 by jsaariko      #+#    #+#                 */
+/*   Updated: 2020/10/14 12:06:29 by jsaariko      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+void	parse_input(const char *input);
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/14 11:59:41 by jsaariko      #+#    #+#                 */
+/*   Updated: 2020/10/14 12:22:34 by jsaariko      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "minishell.h"
+
+void	get_input(void)
+{
+	char *buf;
+
+	// ft_printf("sup? ");
+	write(1, "\U0001F950", 4);
+	write(1, " ", 1);
+	get_next_line(1, &buf);
+	parse_input(buf);
+}
+
+int		main(void)
+{
+	while (1)
+		get_input();
+	return (0);
+}
