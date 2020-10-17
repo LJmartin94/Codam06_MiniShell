add1925b4872975eb821413f16329fe124039f34
limartin <lindsayjamesmartin94@gmail.com>
2020-10-17 09:46:00 +0200

    17 October 2020, Made the global variable static, uglified but suppressed an unused return value error in main.c, added a new line to the end of file of fsm.c. Not much of a commit haha, but want to handle the pull request Jules made now so thought I'd push what I have.

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/14 11:59:41 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/15 18:06:51 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/19 16:04:28 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 #include "parse.h"
 // #include "minishell.h"
 
-void	get_input(void)
+int	get_input(void)
 {
 	char *buf;
+	int ret;
 
 	// ft_printf("sup? ");
-	write(1, "\U0001F40C", 4);
-	write(1, " ", 1);
+	ret = write(1, "\U0001F40C", 4);
+	ret = write(1, " ", 1);
 	get_next_line(1, &buf);
 	parse_input(buf);
+	return (ret);
 }
 
 int		main(void)
