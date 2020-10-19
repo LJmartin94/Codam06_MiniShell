8f8fae97658dffe53181dcb3659bc6678ec87322
saarikoski-jules <jsaariko@student.codam.nl>
2020-10-19 18:13:45 +0200

    Now it compiles as well as being up to date with master

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/14 11:59:41 by jsaariko      #+#    #+#                 */
-<<<<<<< HEAD
-/*   Updated: 2020/10/16 17:32:01 by jsaariko      ########   odam.nl         */
-=======
-/*   Updated: 2020/10/19 15:52:31 by jsaariko      ########   odam.nl         */
->>>>>>> master
+/*   Updated: 2020/10/19 18:09:11 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
--- srcs/parse_input.c
+++ srcs/parse_input.c
@@ -6,11 +6,12 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/14 12:04:04 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/19 12:19:47 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/19 18:09:41 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "parse.h"
 
 void	parse_input(const char *input)
 {
