73db7f84ca43ee6a475dfd2148d0c33a29d70b37
saarikoski-jules <jsaariko@student.codam.nl>
2020-10-19 12:12:56 +0200

    Correct path to repo added to action

--- .github/workflows/check-norme.sh
+++ .github/workflows/check-norme.sh
@@ -1,2 +1,2 @@
-val=$(~/norminette/norminette.rb ~/work/testing_actions/testing_actions/ | grep "Error" | grep -v -c "Error: 42 header not at top of the file")
+val=$(~/norminette/norminette.rb ~/work/Codam06_MiniShell/Codam06_MiniShell/ | grep "Error" | grep -v -c "Error: 42 header not at top of the file")
 exit $val
