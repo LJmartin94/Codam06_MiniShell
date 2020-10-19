fde9cf0ae4c019023b7c64ef1b3376107a1c7c4f
saarikoski-jules <jsaariko@student.codam.nl>
2020-10-19 18:16:27 +0200

    ..now it compiles

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/14 14:04:54 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/19 18:11:48 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/19 18:15:53 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,6 @@ void				add_token(t_token **head, t_token *token);
 t_token				*create_token(char *tokens, int j, size_t len);
 void				free_matrix(char **tokens);
 void				print_tokens(t_token **tokens);
+void				parse_input(const char *input);
 
 #endif
