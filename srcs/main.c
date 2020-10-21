e9ed61caf0b4579f930c68088233ebc4a29e69ef
saarikoski-jules <jsaariko@student.codam.nl>
2020-10-21 10:44:15 +0200

    Handling sigquit

--- srcs/main.c
+++ srcs/main.c
@@ -6,24 +6,24 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/14 11:59:41 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/20 18:45:16 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/21 10:43:51 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "parse.h"
-#include<signal.h>
+#include <signal.h>
 
 void	get_input(void)
 {
-	char *buf;
-	int ret;
+	char	*buf;
+	int		ret;
 
 	write(1, "\U0001F40C ", 6);
 	ret = get_next_line(STDIN_FILENO, &buf);
-	if (ret == 0)	//ctrl-D
+	if (ret == 0)
 	{
-		ft_printf("\n"); //TODO: try not to print ^D
+		ft_printf("\n");
 		exit(0);
 	}
 	parse_input(buf);
@@ -35,7 +35,6 @@ void sig_handler(int signo)
 	(void)signo;
 	write(1, "\n", 1);
 	write(1, "\U0001F40C ", 6);
-	// write(1, " ", 1);
 }
 
 void other(int signo)
@@ -47,7 +46,7 @@ void other(int signo)
 int		main(void)
 {
 	signal(SIGINT, sig_handler);
-	signal(SIGQUIT, other);
+	signal(SIGQUIT, SIG_IGN);
 	while(1)
 		get_input();
 	return (0);
