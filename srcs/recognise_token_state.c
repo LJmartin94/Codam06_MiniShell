8ab03dad532714741a1bcfaba43ed75658a79ec4
LJmartin94 <lindsayjamesmartin94@gmail.com>
2020-10-22 13:43:33 +0200

    22 October 2020, Made it no longer do an heccin crash when no input is given lmao oops.

--- srcs/recognise_token_state.c
+++ srcs/recognise_token_state.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/20 15:11:21 by limartin      #+#    #+#                 */
-/*   Updated: 2020/10/21 15:35:52 by limartin      ########   odam.nl         */
+/*   Updated: 2020/10/22 13:41:05 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ t_transition_code	recognise_token_state(t_token *this)
 	int					i;
 	t_transition_code	id;
 
+	if (this == NULL)
+		return(exit_state);
 	i = 0;
 	id = arg;
 	while (i < RECOG_TABLE_SIZE)
