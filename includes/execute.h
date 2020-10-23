f71c39f295d71729c97132b583c747e309dbd4c8
saarikoski-jules <jules.saarikoski@gmail.com>
2020-10-23 10:27:10 +0200

    env command doesn't crash (y)

--- includes/execute.h
+++ includes/execute.h
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/22 13:40:30 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/22 17:26:10 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/23 09:31:06 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
