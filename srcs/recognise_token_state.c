076490c1ade37113fd7f3fd3a30a6daf5ba8a016
LJmartin94 <lindsayjamesmartin94@gmail.com>
2020-10-23 16:11:01 +0200

    23 October 2020, Everything okayed by norminette and norminette plus, though I had to readd the new libft submodule and now apparently it has 'new commits' because the 'HEAD detached at 7d444ec', which I think(?) is just a fancy way of saying that this will be the libft version we will use for this project untill further notice? Regardless of whether libft ggets updated in the meantime? But wasn't sure. Will now probably start working on a 'padding' state, which means revisiting the ft_split approach we have been using so far in the tokenizer.

--- srcs/recognise_token_state.c
+++ srcs/recognise_token_state.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/20 15:11:21 by limartin      #+#    #+#                 */
-/*   Updated: 2020/10/22 13:41:05 by limartin      ########   odam.nl         */
+/*   Updated: 2020/10/23 16:02:52 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_transition_code	recognise_token_state(t_token *this)
 	t_transition_code	id;
 
 	if (this == NULL)
-		return(exit_state);
+		return (exit_state);
 	i = 0;
 	id = arg;
 	while (i < RECOG_TABLE_SIZE)
