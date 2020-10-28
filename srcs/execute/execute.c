ebd6c44ea6d87bf3255b4a51335d414a0c80afd6
saarikoski-jules <jules.saarikoski@gmail.com>
2020-10-28 17:13:37 +0100

    looping over commands

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/22 16:32:46 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/28 15:53:24 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/28 16:56:06 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,15 @@ t_cmd	get_command(t_icomp *comp)
 
 void	execute(t_icomp *comp)
 {
-	t_cmd f;
+	t_cmd	f;
+	t_icomp	*tmp;
 
-	f = get_command(comp);
-	if (f != NULL)
-		f(comp);
+	tmp = comp;
+	while (tmp != NULL)
+	{
+		// ft_dprintf(STDIN_FILENO, "cur command: %s\nnext addr: %p\n", tmp->cmd, tmp->right);
+		f = get_command(tmp);
+		f(tmp);
+		tmp = tmp->right;
+	}
 }
