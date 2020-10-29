9bd160b0869005d8c8a6376e0c82d368bcecdc2b
saarikoski-jules <jules.saarikoski@gmail.com>
2020-10-29 18:09:41 +0100

    segfaults. Reading dirs and executing commands just fine

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,12 +6,15 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/22 16:32:46 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/10/28 18:30:11 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/10/29 17:32:33 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 #include "libft.h"
+#include <unistd.h>
+#include "error.h"
+#include <dirent.h>
 
 /*
 ** TODO: Build command validators in functions
@@ -30,6 +33,101 @@ t_cmd	get_command(t_icomp *comp)
 	return (NULL);
 }
 
+char	**build_argv(t_icomp *comp)
+{
+	int i = 0;
+	int j = 0;
+	char **try = ft_split(comp->arg, ' ');
+	while(try[i] != NULL)
+	{
+		ft_dprintf(STDIN_FILENO, "%s, %d\n", try[i], i);
+		i++;
+	}
+	char **argv = (char **)e_malloc(i + 1);
+	argv[j] = comp->cmd;//change
+	j++;
+	i = 0;
+	while(try[i] != NULL)
+	{
+		ft_dprintf(STDIN_FILENO, "lol %d %d\n", i, j);
+		argv[j] = try[i];
+		i++;
+		j++;
+	}
+	argv[j] = NULL;
+	//free matrix
+	return (argv);
+}
+
+char	*readdir_test(t_vector *env, t_icomp *comp)
+{
+	t_env *item = vector_get(env, vector_search(env, compare_key, "PATH"));
+	ft_dprintf(STDIN_FILENO, "%s\n", item->value);
+	char **split = ft_split(item->value, ':');
+	int i;
+	i = 0;
+	char *found;
+	found = NULL;
+	while(split[i] != NULL)
+	{
+		ft_dprintf(STDIN_FILENO, "path part %d: %s\n", i, split[i]);
+		i++;
+	}
+	ft_dprintf(STDIN_FILENO, "DONE\n");
+	DIR *dir;
+	// struct dirent *dp;
+	int j = 0;
+	while(split[j] != NULL)
+	{
+		dir = opendir(split[i]);
+		// dp = readdir(dir);
+		struct dirent *entry;
+		while ((entry = readdir(dir)) != NULL)
+		{
+			if (ft_strncmp(entry->d_name, comp->cmd, ft_strlen(comp->cmd) + 1))
+			{
+				found = ft_strdup(entry->d_name);
+				closedir(dir); //TODO: free all the shit
+				return (found);
+			}
+		}
+		int dir_ret = closedir(dir);
+		
+		ft_dprintf(STDIN_FILENO, "dir_ret: %d\n", dir_ret);
+		j++;
+	}
+	return (NULL);
+	(void)comp;
+}
+
+void	exec_command(t_vector *env, t_icomp *comp)
+{
+	int pid;
+	char **envp;
+	char **argv;
+
+	char *dir= readdir_test(env, comp);
+	if (dir == NULL)
+		invalid_cmd(comp);
+	char *command = ft_strjoin(dir, comp->cmd);
+	ft_dprintf(STDIN_FILENO, "command: %s\n", command);	
+
+	pid = fork();
+	if (pid != 0) //if in parent process
+	{
+		wait(&pid);
+	}
+	else
+	{
+		envp = env_to_envp(env);
+		free_environment(env);
+		argv = build_argv(comp);
+		execve(command, argv,  envp);
+		exit(0);
+	}
+	
+}
+
 void	execute(t_vector *env, t_icomp *comp)
 {
 	t_cmd	f;
@@ -40,7 +138,7 @@ void	execute(t_vector *env, t_icomp *comp)
 	{
 		f = get_command(tmp);
 		if (f == NULL)
-			invalid_cmd(tmp);
+			exec_command(env, comp);
 		else
 			f(env, tmp);
 		tmp = tmp->right;
