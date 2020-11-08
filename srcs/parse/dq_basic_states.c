9a41581948f237db26656e975ec106fb0c910f6e
LJmartin94 <lindsayjamesmartin94@gmail.com>
2020-11-08 16:01:35 +0100

    8 November 2020, Fixed an edge case with handling double and single quotes: empty dqs and sqs will now handle correctly.

--- srcs/parse/dq_basic_states.c
+++ srcs/parse/dq_basic_states.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/15 18:10:03 by limartin      #+#    #+#                 */
-/*   Updated: 2020/11/06 13:53:22 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/11/08 15:38:38 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ t_transition_code	sh_dq_cmd_state(t_token **this, t_icomp **icur)
 
 	if (recognise_token_state(*this) == dq && *this)
 		*this = (*this)->next;
-	ft_add_token_to_comp((*this), &((*icur)->cmd));
-	*this = (*this)->next;
+	if (recognise_token_state(*this) != dq && *this)
+	{
+		ft_add_token_to_comp((*this), &((*icur)->cmd));
+		*this = (*this)->next;
+	}
 	id = exit_state;
 	if ((*this) != NULL)
 		id = recognise_token_state(*this);
@@ -33,8 +36,11 @@ t_transition_code	sh_dq_opt_state(t_token **this, t_icomp **icur)
 
 	if (recognise_token_state(*this) == dq && *this)
 		*this = (*this)->next;
-	ft_add_token_to_comp((*this), &((*icur)->arg));
-	*this = (*this)->next;
+	if (recognise_token_state(*this) != dq && *this)
+	{
+		ft_add_token_to_comp((*this), &((*icur)->arg));
+		*this = (*this)->next;
+	}
 	id = exit_state;
 	if ((*this) != NULL)
 		id = recognise_token_state(*this);
@@ -47,8 +53,11 @@ t_transition_code	sh_dq_arg_state(t_token **this, t_icomp **icur)
 
 	if (recognise_token_state(*this) == dq && *this)
 		*this = (*this)->next;
-	ft_add_token_to_comp((*this), &((*icur)->arg));
-	*this = (*this)->next;
+	if (recognise_token_state(*this) != dq && *this)
+	{
+		ft_add_token_to_comp((*this), &((*icur)->arg));
+		*this = (*this)->next;
+	}
 	id = exit_state;
 	if ((*this) != NULL)
 		id = recognise_token_state(*this);
