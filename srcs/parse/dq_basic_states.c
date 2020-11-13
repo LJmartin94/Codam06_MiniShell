3943671cc3dc3135a5d3924e83c190f36b4811e0
limartin <lindsayjamesmartin94@gmail.com>
2020-11-13 17:27:50 +0100

    13 November 2020, Added argument linked list implementation for single quote basic state and double quote basic state. Still need to implement it for backslashes, double quote backslashes and singlequote backslashes. May call it a day now though

--- srcs/parse/dq_basic_states.c
+++ srcs/parse/dq_basic_states.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/15 18:10:03 by limartin      #+#    #+#                 */
-/*   Updated: 2020/11/13 09:14:34 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/11/13 16:28:22 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,16 +49,37 @@ t_transition_code	sh_dq_opt_state(t_token **this, t_icomp **icur)
 	return (id);
 }
 
+static void			sh_dq_arg_open_state(t_token **this, t_icomp **icur)
+{
+	t_arg	*new;
+
+	if (ft_strlen(((*icur)->arg)->value) > 0)
+	{
+		new = (t_arg *)e_malloc(sizeof(t_arg));
+		ft_argconst(new);
+		ft_add_arg(&(*icur)->arg, new);
+		free(new->type);
+		new->type = ft_strdup("\"");
+		if (new->type == NULL)
+			error_exit_errno();
+	}
+	*this = (*this)->next;
+}
+
 t_transition_code	sh_dq_arg_state(t_token **this, t_icomp **icur)
 {
 	t_transition_code	id;
+	t_arg				*last;
 
 	if (recognise_token_state(*this) == dq && *this)
-		*this = (*this)->next;
+		sh_dq_arg_open_state(this, icur);
+	last = (*icur)->arg;
+	while (last->right != NULL)
+		last = last->right;
 	if (recognise_token_state(*this) != backslash && \
 	recognise_token_state(*this) != dq && *this)
 	{
-		ft_add_token_to_comp((*this), &(((*icur)->arg)->value));
+		ft_add_token_to_comp((*this), &((last)->value));
 		*this = (*this)->next;
 	}
 	id = exit_state;
