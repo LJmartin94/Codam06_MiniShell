4d83fa614484be2d51e91cc34563c4ad2d2b83fa
saarikoski-jules <jules.saarikoski@gmail.com>
2020-11-13 13:59:58 +0100

    exits with exit value of last command if no arg provided

--- srcs/execute/exit.c
+++ srcs/execute/exit.c
@@ -6,25 +6,19 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/09 14:28:00 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/11/11 15:10:04 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/11/13 13:59:27 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 
-//TODO: If numeric param provided, exits with said param
-//TODO: If not, exits with exit code of last command (g_ret_val)
-//TODO: what if non numeric param provided? errors, still exits, but what exit value?
-
 int	ft_exit(t_vector *env, t_icomp *cmd, int fd) 
 {
-	(void)env;
-	(void)fd;
-	size_t i;
+	size_t	i;
+	
 	i = 0;
-	// int ret; //TODO: Maybe unsigned char?
-	// if (cmd->arg[0] == '\0')
-		// exit(g_ret_val);
+	if (cmd->arg[i] == '\0')
+		exit(g_ret_val);
 	while (cmd->arg[i] != '\0')
 	{
 		if (!ft_isdigit(cmd->arg[i]))
@@ -34,5 +28,7 @@ int	ft_exit(t_vector *env, t_icomp *cmd, int fd)
 		}
 		i++;
 	}
+	(void)env;
+	(void)fd;
 	exit(ft_atoi(cmd->arg));
 }
