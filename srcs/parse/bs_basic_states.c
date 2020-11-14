f6cf6ba8e99483f5732517b5e98b55313ccd2b06
limartin <lindsayjamesmartin94@gmail.com>
2020-11-14 17:07:11 +0100

    14 November 2020, Added the same implementation of linked list arguments to the backslash and double quote backslash states as I had already done for all the other basic state variants. Did however run in to an issue, as the option state can sometimes be responsible for parsing arguments (due to actual legitimate reasons rather than hacky code haha), so now need to implement something similar for option state when dealing with args.

--- srcs/parse/bs_basic_states.c
+++ srcs/parse/bs_basic_states.c
@@ -6,11 +6,13 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/05 18:05:53 by limartin      #+#    #+#                 */
-/*   Updated: 2020/11/13 09:21:21 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/11/14 16:01:00 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
+#include "error.h"
+#include "libft.h"
 
 t_transition_code	sh_bs_cmd_state(t_token **this, t_icomp **icur)
 {
@@ -46,15 +48,36 @@ t_transition_code	sh_bs_opt_state(t_token **this, t_icomp **icur)
 	return (id);
 }
 
+static void			sh_bs_arg_link_state(t_token **this, t_icomp **icur)
+{
+	t_arg	*new;
+
+	if (ft_strlen(((*icur)->arg)->value) > 0)
+	{
+		new = (t_arg *)e_malloc(sizeof(t_arg));
+		ft_argconst(new);
+		ft_add_arg(&(*icur)->arg, new);
+		free(new->type);
+		new->type = ft_strdup("\\");
+		if (new->type == NULL)
+			error_exit_errno();
+	}
+	*this = (*this)->next;
+}
+
 t_transition_code	sh_bs_arg_state(t_token **this, t_icomp **icur)
 {
 	t_transition_code	id;
+	t_arg				*last;
 
 	if (recognise_token_state(*this) == backslash && *this)
-		*this = (*this)->next;
+		sh_bs_arg_link_state(this, icur);
+	last = (*icur)->arg;
+	while (last->right != NULL)
+		last = last->right;
 	if ((*this) != NULL)
 	{
-		ft_add_token_to_comp((*this), &(((*icur)->arg)->value));
+		ft_add_token_to_comp((*this), &((last)->value));
 		*this = (*this)->next;
 	}
 	id = exit_state;
--- srcs/parse/dq_bs_basic_states.c
+++ srcs/parse/dq_bs_basic_states.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/08 16:22:46 by limartin      #+#    #+#                 */
-/*   Updated: 2020/11/13 09:16:02 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/11/14 16:47:57 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,15 +46,36 @@ t_transition_code	sh_dq_bs_opt_state(t_token **this, t_icomp **icur)
 	return (id);
 }
 
+static void			sh_dq_bs_arg_link_state(t_token **this, t_icomp **icur)
+{
+	t_arg	*new;
+
+	if (ft_strlen(((*icur)->arg)->value) > 0)
+	{
+		new = (t_arg *)e_malloc(sizeof(t_arg));
+		ft_argconst(new);
+		ft_add_arg(&(*icur)->arg, new);
+		free(new->type);
+		new->type = ft_strdup("\"\\");
+		if (new->type == NULL)
+			error_exit_errno();
+	}
+	*this = (*this)->next;
+}
+
 t_transition_code	sh_dq_bs_arg_state(t_token **this, t_icomp **icur)
 {
 	t_transition_code	id;
+	t_arg				*last;
 
 	if (recognise_token_state(*this) == backslash && *this)
-		*this = (*this)->next;
+		sh_bs_arg_link_state(this, icur);
+	last = (*icur)->arg;
+	while (last->right != NULL)
+		last = last->right;
 	if ((*this) != NULL)
 	{
-		ft_add_token_to_comp((*this), &(((*icur)->arg)->value));
+		ft_add_token_to_comp((*this), &((last)->value));
 		*this = (*this)->next;
 	}
 	id = exit_state;
