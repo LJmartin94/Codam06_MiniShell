e114c7b1f26001d94fe57c06ce86c626de3b2916
limartin <lindsayjamesmartin94@gmail.com>
2020-11-25 18:02:02 +0100

    25 November 2020, Simplified validate_option_flags.c somewhat already, including the removal of the MULTICHAR option flag handling function, which handled the '--newline' option we had made up alongside '-n'. This may have still been working though, but I didn't test whether it was and didn't want to leave it in as an extra liability in case it wasn't.

--- srcs/parse/validate_option_flags.c
+++ srcs/parse/validate_option_flags.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/25 17:19:17 by limartin      #+#    #+#                 */
-/*   Updated: 2020/11/25 15:18:19 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/11/25 17:58:01 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,55 +64,6 @@ static int	single_char_flag(t_icomp **icur, int i, int j)
 	return (1);
 }
 
-static int	multi_char_flag(t_icomp **icur, int j)
-{
-	size_t	cmd_len;
-	size_t	opt_len;
-
-	cmd_len = ft_strlen((*icur)->cmd);
-	opt_len = ft_strlen(((*icur)->arg)->value);
-	if (opt_len < 4)
-		return (0);
-	if (cmd_len != ft_strlen(g_flagvalid_table[j].cmd))
-		return (0);
-	if (ft_strncmp(g_flagvalid_table[j].cmd, (*icur)->cmd, cmd_len))
-		return (0);
-	if (((*icur)->arg)->value[1] != '-')
-		return (0);
-	if (ft_strncmp(g_flagvalid_table[j].flag, ((*icur)->arg)->value + 2, \
-	opt_len))
-		return (0);
-	return (1);
-}
-
-int			old_validate_option_flags(t_icomp **icur)
-{
-	int		i;
-	int		j;
-	int		ret;
-
-	if (ft_strlen(((*icur)->arg)->value) < 2)
-		return (0);
-	i = 1;
-	ret = 1;
-	while (((*icur)->arg)->value[i] != '\0' && ret)
-	{
-		ret = 0;
-		j = 0;
-		while (j < FLAG_TABLE_SIZE)
-		{
-			if (ret == 1 || single_char_flag(icur, i, j) || \
-			multi_char_flag(icur, j))
-				ret = 1;
-			j++;
-		}
-		i++;
-	}
-	if (ret)
-		ft_approve_option(icur);
-	return (ret);
-}
-
 int			validate_option_flags(t_icomp **icur)
 {
 	int		i;
@@ -123,7 +74,6 @@ int			validate_option_flags(t_icomp **icur)
 	i = 0;
 	ret = 1;
 	link = (*icur)->arg;
-	//check if first char of first link value is -
 	if (link->value[0] == '\0')
 		return (0);
 	if ((*icur)->arg->value[0] == '-')
@@ -140,8 +90,7 @@ int			validate_option_flags(t_icomp **icur)
 			j = 0;
 			while (j < FLAG_TABLE_SIZE)
 			{
-				if (ret == 1 || single_char_flag(icur, i, j) || \
-				multi_char_flag(icur, j))
+				if (ret == 1 || single_char_flag(icur, i, j))
 					ret = 1;
 				j++;
 			}
@@ -153,10 +102,7 @@ int			validate_option_flags(t_icomp **icur)
 			break ;
 	}
 	ret--;
-	i = (link != (*icur)->arg || ft_strlen ((link)->value) >= 2) ? 1 : 0;
-	printf("link == |%s|\n", link->value);
-	printf("pad == |%s|\n", link->pad);
-	printf("retb4 == %d\n", ret);
+	i = (link != (*icur)->arg || ft_strlen((link)->value) >= 2) ? 1 : 0;
 	if (ret == 0 && i)
 	{
 		ft_approve_option(icur);
@@ -164,14 +110,5 @@ int			validate_option_flags(t_icomp **icur)
 	}
 	else if (ret == 0 && link->pad[0] != '\0')
 		ret--;
-	printf("ret == %d\n\n", ret);
-	
-	
-	// if (ret == 1 && link->pad[0] == '\0')
-	// 	ret = 0;
-	// else if (ret == 0) 
-	// 	ret = -1;
-	// else if (ret == 1)
-	// 	ft_approve_option(icur);
 	return (ret);
 }
