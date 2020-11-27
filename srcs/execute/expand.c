a06c8f3c1f331596655075d0e525abbb9c40de39
saarikoski-jules <jules.saarikoski@gmail.com>
2020-11-27 16:05:15 +0100

    cleaned up expansions

--- srcs/execute/expand.c
+++ srcs/execute/expand.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/13 12:51:50 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/11/27 15:57:34 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/11/27 16:03:33 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,10 @@ static t_env	*expand_find(t_vector *env, char *pos)
 	int		index;
 	t_env	*item;
 
-	//charset !is_alnum
-	// len = ft_strchrset(pos, " \"\'-");//TODO: add whatever other chars that will be parsed out eventually
 	len = 0;
-	while(ft_isalnum(pos[len]) || pos[len] == '_')
-	{
+	while (ft_isalnum(pos[len]) || pos[len] == '_')
 		len++;
-	}
-	// ft_dprintf(STDOUT_FILENO, "%d\n", len);
-	if (len >= 0)//TODO: Maybe change back to (len > 0)
+	if (len >= 0)
 	{
 		key = (char *)e_malloc(sizeof(char) * len + 1);
 		key = ft_memcpy(key, pos, sizeof(char) * len);
@@ -58,13 +53,6 @@ static void		replace_arg(t_vector *env, char **str, size_t *i, int quote, int dq
 	t_env	*replace;
 
 	final = NULL;
-	// ft_dprintf(STDOUT_FILENO, "here? [%s] [%d]\n", *str, *i);
-			// (*str)[*i + 1] == '\'' ||
-			// (*str)[*i + 1] == '\"' ||
-			// (*str)[*i + 1] == ' ' ||
-			// (*str)[*i + 1] == '/') //TODO: wtf is / //TODO: I assume this is correct
-			//TODO: Is alpha or is alnum??
-	// if ((*str)[*i + 1] == '"')
 	if ((*str)[*i + 1] == '?')
 	{
 		final = ft_strsplice((*str), *i, 2, ft_itoa(g_ret_val));
@@ -78,54 +66,18 @@ static void		replace_arg(t_vector *env, char **str, size_t *i, int quote, int dq
 			(*str)[*i + 1] == '*' ||
 			ft_isalnum((*str)[*i + 1]))
 	{
-		// ft_dprintf(STDOUT_FILENO, "replacing [%s]\n", (*str) + *i);
 		replace = expand_find(env, (*str) + *i + 1);
 		final = ft_strsplice((*str), *i,
 					ft_strlen(replace->key) + 1,
 					replace->value);
 		free(*str);
 		(*str) = final;
-		// ft_dprintf(STDOUT_FILENO, "\n[%s] + [%s]\n", replace->key, replace->value);
-		// ft_dprintf(STDOUT_FILENO, "i = %d - %d + %d\n\n", *i, ft_strlen(replace->key) + 1, ft_strlen(replace->value));
 		*i = *i + ft_strlen(replace->value) - 1;
-		// *i = *i - (ft_strlen(replace->key) + 1) + ft_strlen(replace->value);
 	}
 	else
-	{
-		//should not go here if next char is " '
-		// ft_dprintf(STDOUT_FILENO, "hereee\n");
-		//TODO: this should work..? echo "hallo$"
-		//TODO: also ignore if next char is a quote of some form
-		//TODO: Test [echo /$"123$USER"]
 		(*i)++;
-		return ;
-	}
 }
 
-// void expand_env_arg(t_vector *env, t_icomp *cmd)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (cmd->cmd[i] != '\0')
-// 	{
-// 		if (cmd->cmd[i] == '$')
-// 			replace_arg(env, &(cmd->cmd), &i);
-// 		else
-// 			i++;
-// 	}
-// 	i = 0;
-// 	while (cmd->arg[i] != '\0')
-// 	{
-// 		if (cmd->arg[i] == '$')
-// 			replace_arg(env, &(cmd->arg), &i);
-// 		else
-// 			i++;
-// 	}
-// }
-
-// TODO: if between single quotes, do not expand
-// TODO: this should work?? echo '$USER'$USER\$USER/"$USER"
 void	expand_env(t_vector *env, char **str)
 {
 	size_t	i;
@@ -136,7 +88,6 @@ void	expand_env(t_vector *env, char **str)
 	int esc = 0;
 	while ((*str)[i] != '\0')
 	{
-		// ft_dprintf(STDOUT_FILENO, "cur char: [%c] i: %d\n", (*str)[i], i);
 		if ((*str)[i] == '"')
 		{
 			if (quote % 2 == 0)
@@ -151,27 +102,20 @@ void	expand_env(t_vector *env, char **str)
 			else
 				(quote)--;
 		}
-		if ((*str)[i] == '$' && quote % 2 == 0 && esc % 2 == 0) //TODO: make sure this doesnt crash
-		{
+		if ((*str)[i] == '$' && quote % 2 == 0 && esc % 2 == 0)
 			replace_arg(env, str, &i, quote, dquote);
-		}
 		else
 			i++;
 		if (i - 1 > 0)
 		{
-			if ((*str)[i - 1] == '\\')// This only applies when \ are next to each other
+			if ((*str)[i - 1] == '\\')
 				esc++;
-			if ((*str)[i - 1] != '\\')// reset esc if cur char is not esc //TODO: Make sure this wont be negative
+			if ((*str)[i - 1] != '\\')
 				esc = 0;
 		}
 	}
 }
 
 // NOTES: Not handling $* or $# as they're not mandatory
-
-
-//echo "\\$USER" <- HOWW
-
-// I still don't know whether to expand or not expand on @ and # ass next chars
 
 //what should i do with numbers.. And special chars like @ #...
