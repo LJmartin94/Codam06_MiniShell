1d5f99a35a9b9105ba09d0940e6e02557c93a2b3
saarikoski-jules <jules.saarikoski@gmail.com>
2020-11-27 14:30:25 +0100

    Expands mainly correctly, except for escaping escape characters

--- srcs/execute/expand.c
+++ srcs/execute/expand.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/13 12:51:50 by jsaariko      #+#    #+#                 */
-/*   Updated: 2020/11/26 14:55:26 by jsaariko      ########   odam.nl         */
+/*   Updated: 2020/11/27 14:24:35 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,15 @@ static t_env	*expand_find(t_vector *env, char *pos)
 	int		index;
 	t_env	*item;
 
-	len = ft_strchrset(pos, " \"\'-");//TODO: add whatever other chars that will be parsed out eventually
-	if (len > 0)//TODO: Maybe change back to (len > 0)
+	//charset !is_alnum
+	// len = ft_strchrset(pos, " \"\'-");//TODO: add whatever other chars that will be parsed out eventually
+	len = 0;
+	while(ft_isalnum(pos[len]) || pos[len] == '_')
+	{
+		len++;
+	}
+	// ft_dprintf(STDOUT_FILENO, "%d\n", len);
+	if (len >= 0)//TODO: Maybe change back to (len > 0)
 	{
 		key = (char *)e_malloc(sizeof(char) * len + 1);
 		key = ft_memcpy(key, pos, sizeof(char) * len);
@@ -45,65 +52,75 @@ static t_env	*expand_find(t_vector *env, char *pos)
 	return (item);
 }
 
-static void		replace_arg(t_vector *env, char **str, size_t *i)
+static void		replace_arg(t_vector *env, char **str, size_t *i, int quote, int dquote)
 {
 	char	*final;
 	t_env	*replace;
 
 	final = NULL;
 	// ft_dprintf(STDOUT_FILENO, "here? [%s] [%d]\n", *str, *i);
-	if (!ft_isprint((*str)[*i + 1]) ||
-			(*str)[*i + 1] == '\"' ||
-			(*str)[*i + 1] == '\'' ||
-			(*str)[*i + 1] == ' ' ||
-			(*str)[*i + 1] == '/') //TODO: wtf is / //TODO: I assume this is correct
-	{
-		//TODO: also ignore if next char is a quote of some form
-		//TODO: Test [echo /$"123$USER"]
-		(*i)++;
-		return ;
-	}
-	else if ((*str)[*i + 1] == '?')
+			// (*str)[*i + 1] == '\'' ||
+			// (*str)[*i + 1] == '\"' ||
+			// (*str)[*i + 1] == ' ' ||
+			// (*str)[*i + 1] == '/') //TODO: wtf is / //TODO: I assume this is correct
+			//TODO: Is alpha or is alnum??
+	// if ((*str)[*i + 1] == '"')
+	if ((*str)[*i + 1] == '?')
 	{
 		final = ft_strsplice((*str), *i, 2, ft_itoa(g_ret_val));
 		free(*str);
 		(*str) = final;
 		i += 2;
 	}
-	else
+	else if (((*str)[*i + 1] == '"' && dquote % 2 == 0) ||
+			((*str)[*i + 1] == '\'' && quote % 2 == 0) ||
+			(*str)[*i + 1] == '_' ||
+			(*str)[*i + 1] == '*' ||
+			ft_isalnum((*str)[*i + 1]))
 	{
-		// ft_dprintf(STDOUT_FILENO, "replacing\n");
+		// ft_dprintf(STDOUT_FILENO, "replacing [%s]\n", (*str) + *i);
 		replace = expand_find(env, (*str) + *i + 1);
 		final = ft_strsplice((*str), *i,
 					ft_strlen(replace->key) + 1,
 					replace->value);
 		free(*str);
 		(*str) = final;
+		// ft_dprintf(STDOUT_FILENO, "raplaced: [%s]\n\n", final);
 		*i = *i - (ft_strlen(replace->key) + 1) + ft_strlen(replace->value);
 	}
-}
-
-void expand_env_arg(t_vector *env, t_icomp *cmd)
-{
-	size_t	i;
-
-	i = 0;
-	while (cmd->cmd[i] != '\0')
+	else
 	{
-		if (cmd->cmd[i] == '$')
-			replace_arg(env, &(cmd->cmd), &i);
-		else
-			i++;
-	}
-	i = 0;
-	while (cmd->arg[i] != '\0')
-	{
-		if (cmd->arg[i] == '$')
-			replace_arg(env, &(cmd->arg), &i);
-		else
-			i++;
+		//should not go here if next char is " '
+		// ft_dprintf(STDOUT_FILENO, "hereee\n");
+		//TODO: this should work..? echo "hallo$"
+		//TODO: also ignore if next char is a quote of some form
+		//TODO: Test [echo /$"123$USER"]
+		(*i)++;
+		return ;
 	}
 }
+
+// void expand_env_arg(t_vector *env, t_icomp *cmd)
+// {
+// 	size_t	i;
+
+// 	i = 0;
+// 	while (cmd->cmd[i] != '\0')
+// 	{
+// 		if (cmd->cmd[i] == '$')
+// 			replace_arg(env, &(cmd->cmd), &i);
+// 		else
+// 			i++;
+// 	}
+// 	i = 0;
+// 	while (cmd->arg[i] != '\0')
+// 	{
+// 		if (cmd->arg[i] == '$')
+// 			replace_arg(env, &(cmd->arg), &i);
+// 		else
+// 			i++;
+// 	}
+// }
 
 // TODO: if between single quotes, do not expand
 // TODO: this should work?? echo '$USER'$USER\$USER/"$USER"
@@ -116,7 +133,7 @@ void	expand_env(t_vector *env, char **str)
 	int dquote = 0;
 	while ((*str)[i] != '\0')
 	{
-		ft_dprintf(STDOUT_FILENO, "cur char: [%c] i: %d\n", (*str)[i], i);
+		// ft_dprintf(STDOUT_FILENO, "cur char: [%c] i: %d\n", (*str)[i], i);
 		if ((*str)[i] == '"')
 		{
 			if (quote % 2 == 0)
@@ -131,13 +148,14 @@ void	expand_env(t_vector *env, char **str)
 			else
 				(quote)--;
 		}
-		//TODO: This might be horrendeously wrong
 		if ((*str)[i] == '$' && quote % 2 == 0) //TODO: make sure this doesnt crash
 		{
 			if (i - 1 >= 0 && (*str)[i - 1] != '\\')
-				replace_arg(env, str, &i);
+			{
+				replace_arg(env, str, &i, quote, dquote);
+			}
 			else if (i - 1 < 0)
-				replace_arg(env, str, &i);
+				replace_arg(env, str, &i, quote, dquote);
 			else
 				i++;
 		}
@@ -145,3 +163,12 @@ void	expand_env(t_vector *env, char **str)
 			i++;
 	}
 }
+
+// NOTES: Not handling $* or $# as they're not mandatory
+
+
+//echo "\\$USER" <- HOWW
+
+// I still don't know whether to expand or not expand on @ and # ass next chars
+
+//what should i do with numbers.. And special chars like @ #...
