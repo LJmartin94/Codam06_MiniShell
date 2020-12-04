07ac85d929d73b58dbe684b3f1cee867258278f6
saarikoski-jules <jules.saarikoski@gmail.com>
2020-12-04 16:13:48 +0100

    updating correct path, and actually counting num of errors, oops

--- .github/workflows/check-norme.sh
+++ .github/workflows/check-norme.sh
@@ -1,2 +1,2 @@
-val=$(~/.norminette/norminette.rb ~/work/Codam06_MiniShell/Codam06_MiniShell/ | grep "Error")
+val=$(~/.norminette/norminette.rb ~/work/Codam06_MiniShell/Codam06_MiniShell/ | grep -c "Error")
 exit $val
