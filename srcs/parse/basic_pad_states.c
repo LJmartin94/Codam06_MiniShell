d205a2485ac95d8453a147a41e51907ce4ce676b
limartin <lindsayjamesmartin94@gmail.com>
2020-12-04 18:54:41 +0100

    4 December 2020, Fixed the weird thing with padding when redirections are involved by changing padding state slightly. Might call it a day soon tho.

--- srcs/parse/basic_pad_states.c
+++ srcs/parse/basic_pad_states.c
@@ -6,7 +6,7 @@
 /*   By: lindsay <lindsay@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/06 13:30:27 by lindsay       #+#    #+#                 */
-/*   Updated: 2020/12/06 16:57:34 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/12/10 19:25:22 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
