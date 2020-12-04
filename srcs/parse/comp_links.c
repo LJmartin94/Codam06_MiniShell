ef9119328ff4de334cc01c9e8d4fee1caf3136d4
limartin <lindsayjamesmartin94@gmail.com>
2020-12-04 18:35:12 +0100

    4 December 2020, Fixed all the memleaks after all, booyah.

--- srcs/parse/comp_links.c
+++ srcs/parse/comp_links.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/19 18:03:33 by limartin      #+#    #+#                 */
-/*   Updated: 2020/12/10 19:34:10 by limartin      ########   odam.nl         */
+/*   Updated: 2021/01/05 10:05:42 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
