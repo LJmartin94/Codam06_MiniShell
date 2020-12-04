e3c46461a0bb8522888202c0f69b3e70d6a212a4
saarikoski-jules <jules.saarikoski@gmail.com>
2020-12-04 16:00:54 +0100

    swapped norminette to now available codam repo in order to also check header files

--- .github/workflows/check-norme.sh
+++ .github/workflows/check-norme.sh
@@ -1,2 +1,2 @@
-val=$(~/norminette/norminette.rb ~/work/Codam06_MiniShell/Codam06_MiniShell/ | grep "Error" | grep -v -c "Error: 42 header not at top of the file")
+val=$(~/norminette/norminette.rb ~/work/Codam06_MiniShell/Codam06_MiniShell/ | grep "Error")
 exit $val
