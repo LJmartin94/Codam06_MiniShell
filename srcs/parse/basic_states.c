bab76025b254c80e1e0bdf056648b46a9926c768
limartin <lindsayjamesmartin94@gmail.com>
2020-12-06 17:23:17 +0100

    6 December 2020, Just realised I hadn't pushed one of the files I changed because I hadn't saved it properly. From initial testing everything seems to be working as intended though, but still need to check out the specific case I was curious about: How do options handle after redirections? Can you have a valid option split up by a redirection occuring in the middle of it (e.g. ->redirection.txtn ?) Will test that about-fuckery next.

--- srcs/parse/basic_states.c
+++ srcs/parse/basic_states.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/15 18:10:03 by limartin      #+#    #+#                 */
-/*   Updated: 2021/01/06 15:37:25 by lindsay       ########   odam.nl         */
+/*   Updated: 2021/01/06 15:38:08 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
