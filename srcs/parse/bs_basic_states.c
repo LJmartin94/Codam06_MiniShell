1c64120954df682858e260698ac2d372c3a6fdbb
limartin <lindsayjamesmartin94@gmail.com>
2020-12-06 17:10:46 +0100

    6 December 2020, Made a bunch of changes in all the states responsible for adding a link to the Arg LL. Also found something in the redir_control_states.c that I still want to double check, so left a comment there. Will start testing a bunch of stuff now.

--- srcs/parse/bs_basic_states.c
+++ srcs/parse/bs_basic_states.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/05 18:05:53 by limartin      #+#    #+#                 */
-/*   Updated: 2020/11/28 17:03:26 by limartin      ########   odam.nl         */
+/*   Updated: 2020/12/06 17:01:55 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ static void			sh_bs_arg_link_state(t_token **this, t_icomp **icur)
 {
 	t_arg	*new;
 
-	if (ft_strlen(((*icur)->arg)->value) > 0)
+	if (ft_strlen(((*icur)->arg)->value) > 0 || \
+	ft_strlen(((*icur)->arg)->type) > 0)
 	{
 		new = (t_arg *)e_malloc(sizeof(t_arg));
 		ft_argconst(new);
