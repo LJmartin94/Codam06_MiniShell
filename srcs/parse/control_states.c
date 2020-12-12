d169d21758fcdea2ff9de29bb6da2e965dc86f5f
limartin <lindsayjamesmartin94@gmail.com>
2020-12-12 17:11:31 +0100

    12 December 2020, Memleaks in cd and pwd are indeed completely resolved as far as I can tell. Have been messing about with sytax errors again, and after lots of debugging realised the weird behaviour is not down to the behaviour of the sates themselves nor the logic that describes what states leads in to what, rather it is the string of tokens that it is being fed that is incomplete, and the TOKENIZER needs to be fixed. Want to push this version first however before I mess anything up.

--- srcs/parse/control_states.c
+++ srcs/parse/control_states.c
@@ -6,17 +6,22 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/15 18:10:03 by limartin      #+#    #+#                 */
-/*   Updated: 2020/12/11 14:46:23 by lindsay       ########   odam.nl         */
+/*   Updated: 2020/12/12 17:06:00 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
+//TODO: remove
+#include "error.h"
+
 t_transition_code	sh_entry_state(t_token **this, t_icomp **icur)
 {
 	t_transition_code	id;
 
-	//write(1, "ENTRY\n", 6);
+	e_write(STDERR_FILENO, "Token in entry state: ", 22);
+	print_tokens(*this);
+	e_write(STDERR_FILENO, "\n", 1);
 	(void)icur;
 	while (recognise_token_state(*this) == padding && *this)
 		*this = (*this)->next;
@@ -30,7 +35,9 @@ t_transition_code	sh_error_state(t_token **this, t_icomp **icur)
 {
 	t_transition_code	id;
 
-	//write(1, "ERROR\n", 6);
+	e_write(STDERR_FILENO, "Token in error state: ", 22);
+	print_tokens(*this);
+	e_write(STDERR_FILENO, "\n", 1);
 	(void)icur;
 	// if (*this != NULL)
 	// 	*this = (*this)->next;
@@ -48,7 +55,6 @@ t_transition_code	sh_exit_state(t_token **this, t_icomp **icur)
 {
 	t_transition_code	id;
 
-	//write(1, "EXIT\n", 5);
 	validate_option_flags(icur);
 	while (*this)
 		*this = (*this)->next;
