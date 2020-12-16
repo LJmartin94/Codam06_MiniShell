7673356fced5ae799561b98c5814ab7d2b4051a5
limartin <lindsayjamesmartin94@gmail.com>
2020-12-16 21:51:10 +0100

    16 December 2020, Not the nicest solution but I managed to make the executor not run if a syntax error is encountered by storing the value 'E' in the argument type of the first component block whenever an error is encountered, and then making the executor check for that.

--- srcs/parse/control_states.c
+++ srcs/parse/control_states.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/15 18:10:03 by limartin      #+#    #+#                 */
-/*   Updated: 2021/01/06 15:50:34 by lindsay       ########   odam.nl         */
+/*   Updated: 2021/01/06 15:50:51 by lindsay       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
