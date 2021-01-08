e8be2f38262ffd0c341ce510cef44c969775cc06
saarikoski-jules <jules.saarikoski@gmail.com>
2021-01-08 16:29:37 +0100

    fixed leaks

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jsaariko <jsaariko@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/14 11:59:41 by jsaariko      #+#    #+#                 */
-/*   Updated: 2021/01/08 13:42:26 by lindsay       ########   odam.nl         */
+/*   Updated: 2021/01/08 16:28:13 by jsaariko      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
